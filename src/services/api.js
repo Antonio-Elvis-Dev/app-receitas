e31189f1996f225bc26920a6dfcf39a090893c5a
Antônio Elvis <102705791+Antonio-Elvis-Dev@users.noreply.github.com>
2023-06-14 20:01:55 -0300

    update

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 //json-server --watch -d 180 --host 10.0.0.101 db.json
 
 const api = axios.create({
-  baseURL: "http://10.0.0.101:3000",
+  baseURL: "http:// 192.168.157.104:3000",
 });
 
 export default api;
